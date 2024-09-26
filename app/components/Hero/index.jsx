7583b34f1005c1846e16980f1e907d0575c56f32
MD.AL MAMUN <mamunofficialmail@gmail.com>
2024-09-26 23:06:22 +0600

    ui-code only

--- app/components/Hero/index.jsx
+++ app/components/Hero/index.jsx
@@ -11,7 +11,7 @@ import Image from "next/image"
 
 
 export default function Hero() {
-    const isSearched = true 
+    const isSearched = false 
     
   return (
     <>
--- app/page.tsx
+++ app/page.tsx
@@ -23,7 +23,6 @@ export default function Home() {
       <Banner/>
       <Blogs/>
       <GuideInformation/>
-      
       <Footer/>
     </main>
   );
