27d2f0e1e4712a4e13e9f340a19c0a7375e27d51
arefin5 <arefintalukder5@gmail.com>
2024-09-30 16:46:42 +0600

    fetch lists

--- redux/store.js
+++ redux/store.js
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { apiSlice } from "./features/api/apiSlice";
 import authSliceReducer from "./features/auth/authSlice";
+import listSlice from "./list/listSlice";
 
 export const store = configureStore({
     reducer: {
         [apiSlice.reducerPath]: apiSlice.reducer,
-        auth: authSliceReducer
+        auth: authSliceReducer,
+        listSlice
     },
     devTools: process.env.NODE_ENV !== 'production',
     middleware: (getDefaultMiddlewares) => 
