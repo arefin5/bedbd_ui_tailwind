59518639501f7919bdc400335db231940a1d9043
arefin5 <arefintalukder5@gmail.com>
2024-10-15 03:39:56 +0600

    password reset

--- app/password/recover/page.jsx
+++ app/password/recover/page.jsx
@@ -1,19 +1,29 @@
 "use client"
 import axiosInstance from '@/redux/services/axiosInstance'
 import Icon from '/components/Icon'
-import { useState } from 'react'
+import { useState,useEffect } from 'react'
 import { forgetpass } from '@/redux/features/auth/authSlice';
 import { useDispatch ,useSelector} from 'react-redux';
+import { useRouter } from 'next/navigation'; 
 
 export default function page() {
     const [email,setEmail]=useState("");
     const { useemail, loading, error } = useSelector((state) => state.auth);
+    const router = useRouter();
+
 // password/recover
 const dispatch = useDispatch();
     const forgetPass=(e)=>{
     e.preventDefault();
     dispatch(forgetpass({ email }));
   };
+
+  useEffect(() => {
+        if (useemail) {
+            router.push('/password/opt'); 
+        }
+    }, [useemail, router]);
+
     return (
         <div className='modal-background'>
             <div className='pt-19 pb-20 sm:pb-24 px-14 sm:px-24 bg-white w-screen max-w-screen-md | absolute-center rounded-10px'>
--- app/password/reset/page.jsx
+++ app/password/reset/page.jsx
@@ -1,22 +1,53 @@
+"use client"
+import { useState } from 'react'
+import { forgetpass } from '@/redux/features/auth/authSlice';
+import { useDispatch ,useSelector} from 'react-redux';
+import { useRouter } from 'next/navigation'; 
+import { resetPasseord } from '@/redux/features/auth/authSlice';
+
 import Icon from '/components/Icon'
 
 export default function page() {
+    const [password,setPassword]=useState("");
+    const [conPass,setConPass]=useState("")
+    const { user, loading, error,token } = useSelector((state) => state.auth);
+
+    const dispatch = useDispatch();
+    
+    const hadleReset=async(e)=>{
+        try{
+            e.preventDefault();
+            console.log("ture")
+            if(password!==conPass)return 
+            if (password !== conPass) return;
+        dispatch(resetPasseord({  password }));
+      
+        }catch(error){
+            console.log(error)
+        }
+    }
     return (
         <div className='modal-background'>
             <div className='pt-19 pb-20 sm:pb-24 px-14 sm:px-24 bg-white w-screen max-w-screen-md | absolute-center rounded-10px'>
 
                 <h3 className='signin-up-form-title'>Sign up</h3> 
-                <form  className='w-full max-w-lg mb-5 grid gap-y-8 | relative-x-center'>
+                <form  className='w-full max-w-lg mb-5 grid gap-y-8 | relative-x-center' onSubmit={hadleReset}>
                     <div className='relative'>
                         <input
                             className='form-input' 
-                            type='password' name='password' placeholder='New Password'/>
+                            type='password' name='password' placeholder='New Password'
+                                value={password}
+                                onChange={(e) => setPassword(e.target.value)}
+                            />
                         <Icon name='rotate-ccw' className='icon absolute-y-center right-6' />
                     </div>
                     <div className='relative'>
                         <input
                             className='form-input' 
-                            type='password' name='confirm-password' placeholder='Confirm password'/>
+                            type='password' name='confirm-password' placeholder='Confirm password'
+                            value={conPass}
+                            onChange={(e) => setConPass(e.target.value)}
+                            />
                         <Icon name='check' className='icon absolute-y-center right-6' />
                     </div>
                     <button className='btn btn-primary'>Continue</button>
