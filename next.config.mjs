f676df05aa40813ca256515d47b7c2546b3469c9
arefin5 <arefintalukder5@gmail.com>
2024-11-06 15:04:02 +0600

    refector next js config

--- next.config.mjs
+++ next.config.mjs
@@ -1,33 +1,4 @@
 
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   transpilePackages: ['lucide-react'],
-//   images: {
-//     remotePatterns: [
-//       {
-//         protocol: 'https',
-//         hostname: 'res.cloudinary.com',
-//         pathname: '**',
-//       },
-//       {
-//         protocol: 'https',
-//         hostname: 'i.ytimg.com',
-//         pathname: '**',
-//       },
-//       {
-//         protocol: 'https',
-//         hostname: 's3-alpha-sig.figma.com',
-//         pathname: '**',
-//       },
-//     ],
-//   },
-//   reactStrictMode: true,
-//   eslint: {
-//     ignoreDuringBuilds: true,
-//   },
-// };
-
-// export default nextConfig;
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   transpilePackages: ['lucide-react'],
