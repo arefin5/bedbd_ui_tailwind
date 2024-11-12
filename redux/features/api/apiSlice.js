1e897aeda9336bc78b697546f901d1321be54022
arefin5 <arefintalukder5@gmail.com>
2024-11-12 18:05:02 +0600

    fix listing

--- redux/features/api/apiSlice.js
+++ redux/features/api/apiSlice.js
@@ -3,8 +3,8 @@ import { createApi, fetchBaseQuery} from "@reduxjs/toolkit/query/react"
 export const apiSlice = createApi({
     reducerPath:"api",
     baseQuery: fetchBaseQuery({
-        // baseUrl: 'http://145.223.22.239:5001/api'
-        baseUrl: 'http://localhost:5001/api'
+        baseUrl: 'http://145.223.22.239:5001/api'
+        // baseUrl: 'http://localhost:5001/api'
 
     }),
 
@@ -13,21 +13,6 @@ export const apiSlice = createApi({
         getListing: builder.query({
             query: (id)=> `/get-single-list/${id}`
         }),
-        // getListing: builder.query({
-
-        //     // query: (id)=> `/get-single-list/${id}`
-        //     async queryFn(_arg, _queryApi, _extraOptions, fetchWithBQ) {
-        //         console.log('from query')
-        //         console.log(_arg)
-        //         try {
-        //           const response = await axios.get("https://jsonplaceholder.typicode.com/todos/1");
-        //         //   console.log(response.data)
-        //           return { data: response.data };
-        //         } catch (error) {
-        //           return { error: { status: error.response?.status, data: error.message } };
-        //         }
-        //       },
-        // }),
         getListings: builder.query({
             query: ()=> '/all-list'
         })
