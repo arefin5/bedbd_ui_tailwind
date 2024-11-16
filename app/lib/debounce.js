18fb36dec538f642d4a16f6f72fb8222fe089584
arefin5 <arefintalukder5@gmail.com>
2024-11-17 02:57:39 +0600

    fix issue

---
+++ app/lib/debounce.js
@@ -0,0 +1,10 @@
+const debounce = (func, delay=300) => {
+    let timeoutId;
+    return (...args) => {
+      clearTimeout(timeoutId);
+      timeoutId = setTimeout(() => {
+        func(...args);
+      }, delay);
+    };
+  };
+export default debounce
