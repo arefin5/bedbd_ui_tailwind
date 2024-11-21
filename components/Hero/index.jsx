9c0b88614277a6a84226159648a054163e24a103
arefin5 <arefintalukder5@gmail.com>
2024-11-21 23:12:43 +0600

    try to fix

--- components/Hero/index.jsx
+++ components/Hero/index.jsx
@@ -6,6 +6,7 @@ import {  useState, useRef } from "react"
 import {  useSearchBoxCore, useSearchSession } from "@mapbox/search-js-react"
 import LocationMap from "./LocationMap"
 import LocationInput from './LocationInput'
+import { useSelector } from 'react-redux'
 import CheckInOutInput from './CheckInOutInput'
 import GuestCountInput from './GuestCountInput'
 import PropertyTypeInput from './PropertyTypeInput'
@@ -23,31 +24,30 @@ export default function Hero(){
     const mapSearchBox = useSearchBoxCore({ accessToken: 'pk.eyJ1IjoibWQtYWwtbWFtdW4iLCJhIjoiY2x1ZHk1dDZlMWkxdTJqbmlkN2JmZWljaiJ9.YTqqaus6tdGIdJPx5sqlew' })
     const searchSession = new useSearchSession(mapSearchBox);
 
-    const [mapData, setMapData]= useState({
-                                            isMapOpen: true  ,
-                                            isSuggestionMenuOpen: false, 
-                                            suggestions: [],
-                                            selectedSuggestion:{}
-                                        })
-    const [propertyLoactions, setPropertyLoactions] = useState([]);
+    const {  selectedLocation, 
+                    isMapOpen,
+        isSuggestionsMenuOpen,
+                    suggestions
+                } = useSelector(state => state.search.location);
 
-    const handleSearch=(e)=>{
-        e.preventDefault();
-        console.log("search start ",propertyLoactions);
-      // axios.post // http://145.223.22.239:5001/api/sort-by-location?latitude=37.7749&longitude=-122.4194&maxDistance=10000
-
-    }
+    // const [mapData, setMapData]= useState({
+    //                                         isMapOpen: true  ,
+    //                                         isSuggestionMenuOpen: false, 
+    //                                         suggestions: [],
+    //                                         selectedSuggestion:{}
+    //                                     })
+    const [propertyLoactions, setPropertyLoactions] = useState([])
     return (
         <>
             <div className=" relative w-100 md:bg-hero z-10 ">
-                <div className={`${ mapData['isMapOpen']
+                <div className={`${ isMapOpen
                                         ? 'grid md:flex ' 
                                         :'md:relative md:container min-h-96  md:ml-auto md:mr-auto '}`}>
-                    <form className={`${mapData['isMapOpen']
-                                            ? ' md:w-80 md:min-h-screen md:py-8 md:px-6 md:bg-secondary-50'
+                    <form className={`${isMapOpen
+                                            ? ' md:min-w-[calc(theme(minWidth.96)-50px)] md:h-full overflow-y-scroll md:py-8 md:px-6 md:bg-secondary-50 max-h-[calc(100vh-100px)]'
                                             : 'md:absolute-center md:rounded-2xl md:min-h-52 md:w-full md:max-w-5xl md:bg-white md:backdrop-filter md:backdrop-blur-2xl md:bg-opacity-70'}`}>
                         {
-                            mapData['isMapOpen'] &&
+                            isMapOpen &&
                                 <div className=" hidden md:flex  items-center justify-between mb-8 ">
                                     <div className="border font-semibold text-primary-400 text-sm border-primary-400 rounded-full flex py-3 px-6 gap-x-2.5 w-fit">
                                         <Filter className="text-primary-400" size={24} />
@@ -60,50 +60,52 @@ export default function Hero(){
                         }
 
                         <div className={`w-full  space-y-4 px-4 py-6
-                            ${mapData['isMapOpen']
+                            ${isMapOpen
                                 ? 'md:space-y-6 w-[274px]'
                                 : '  | md:max-w-4xl md:rounded-full md:flex md:items-center md:py-3.5 md:pl-8 md:pr-4 md:bg-white md:space-y-0 md:justify-between md:shadow-search-section md:absolute-x-center md:bottom-8'
                                  
                             }`}>
-                            <LocationInput mapSearchBox={mapSearchBox} mapData={mapData} searchSession={searchSession} setMapData={setMapData} mapRef={mapRef} setPropertyLoactions={setPropertyLoactions}  />
-                            <CheckInOutInput mapData={mapData} />
-                            <GuestCountInput mapData={mapData}/>
+                            <LocationInput mapSearchBox={mapSearchBox} searchSession={searchSession} mapRef={mapRef} />
+                            <CheckInOutInput/>
+                            <GuestCountInput />
                             {
-                                !mapData['isMapOpen'] 
+                                !isMapOpen
                                     && <PropertyTypeSelect />
                             }                      
-                            <PropertyTypeInput mapData={mapData}/>
-                            <PropertyViewInput mapData={mapData}/>
-                            <PropertyConditionInput mapData={mapData}/>
-                            <AreaRangeInput mapData={mapData}/>
-                            <PriceRangeInput mapData={mapData}/>
-                            <RatingInput mapData={mapData}/>
-                            <AmenitiesInput mapData={mapData}/>
-                            <BadgesInput mapData={mapData}/>
-                            <GenderPreference mapData={mapData}/>
+                            <PropertyTypeInput />
+
+                            <PropertyViewInput />
+                            <PropertyConditionInput/>
+                            <AreaRangeInput/>
+
+                            <PriceRangeInput/>
+
+                            <RatingInput/>
+
+                            <AmenitiesInput/>
+                            <BadgesInput/>
+                            
+                            <GenderPreference/>
 
                             {/* Submit Button */}
                             <div className='flex pl-14 pr-14 mt-8 space-x-4 md:mt-0 md:p-0 '>
-                                <button className={`btn font-medium text-base text-primary-400 underline ${!mapData['isMapOpen'] && 'md:hidden'} `}>Clear</button>
-                                <button
-                                 onClick={handleSearch}
-                                 className={`btn btn-primary space-x-4 text-center flex items-center justify-center ${!mapData['isMapOpen'] && 'md:h-14 md:w-14 md:rounded-full md:p-auto' }`} >
+                                <button className={`btn font-medium text-base text-primary-400 underline ${!isMapOpen && 'md:hidden'} `}>Clear</button>
+                                <button className={`btn btn-primary space-x-4 text-center flex items-center justify-center ${!isMapOpen && 'md:h-14 md:w-14 md:rounded-full md:p-auto' }`} >
                                     <Search className='icon mr-4 md:m-0 md:h-8 md:w-8'/>
-                                    <span className={` ${ !mapData['isMapOpen'] && 'md:hidden' } `} 
-                                   >Search</span></button>
+                                    <span className={` ${ !isMapOpen && 'md:hidden' } `}>Search</span></button>
                             </div>
                         </div>
                     </form>
                     {
                         // map
-                        mapData['isMapOpen'] 
-                        &&  <div className="p-8 w-full min-h-[600px] md:min-h-full max-h-screen md:max-h-full ">
-                                <LocationMap ref={mapRef}  properties={propertyLoactions} />
+                        isMapOpen
+                        &&  <div className="p-8 w-full min-h-[600px]  md:min-h-full md:max-h-[calc(100vh-80px)]">
+                                <LocationMap ref={mapRef}  />
                             </div>
                     }
                 </div>
             </div>
-            { !mapData['isMapOpen'] && <FilterSection /> }
+            { !isMapOpen && <FilterSection /> }
         </>
     )
 }
