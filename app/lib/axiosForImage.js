c889d80b11a451821da17611d428c6edc97a27e4
arefin5 <arefintalukder5@gmail.com>
2024-12-31 21:22:42 +0600

    test axios

--- app/lib/axiosForImage.js
+++ app/lib/axiosForImage.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const axiosForImage = axios.create({
   baseURL: 'https://backend.bedbd.com/api/',
-  crossOrigin: 'anonymous', // Set this globally for all requests
+  // crossOrigin: 'anonymous', // Set this globally for all requests
 });
 
 export default axiosForImage;
